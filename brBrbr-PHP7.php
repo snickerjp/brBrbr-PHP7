27c4783c38bdad3aedb106255d2eff7d226ee4ae
snickerjp <snickerjp@gmail.com>
2017-11-13 19:40:07 +0900

    mod $1

--- brBrbr-PHP7.php
+++ brBrbr-PHP7.php
@@ -25,7 +25,7 @@ add_filter('comment_text','brBrbr',30);
 function brBrbr($brbr) {
 	$brbr = str_replace(array("\r\n", "\r"), "\n", $brbr); // cross-platform newlines 
 	$brbr = str_replace("\n", "<br />\n", $brbr); // cross-platform newlines 
-	$brbr = preg_replace_callback('!(</?(?:table|img|thead|tfoot|caption|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|form|textarea|input|blockquote|address|p|math|script|h[1-6])[^>]*>)\s*<br />!', function($m) {return "$1";}, $brbr);
+	$brbr = preg_replace_callback('!(</?(?:table|img|thead|tfoot|caption|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|form|textarea|input|blockquote|address|p|math|script|h[1-6])[^>]*>)\s*<br />!', function($m) {return $m[1];}, $brbr);
 	$brbr = preg_replace_callback('|<blockquote([^>]*)>|i', function($m) {return "</p>\n<blockquote".$m[1]."><p>";}, $brbr);
 	$brbr = str_replace('</blockquote>', "</p></blockquote>\n<p>", $brbr);
 	$brbr = preg_replace_callback('/(<pre.*?>)(.*?)<\/pre>/is', function($m) {return clr_br($m[0]);}, $brbr);
