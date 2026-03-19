a254febe3bd4c90f6a90fb46ea27198eb641d914
snickerjp <snickerjp@gmail.com>
2026-03-19 20:48:21 +0900

    v2.1.0: brBrbr v4.1の改善点をマージし可読性を向上
    - <br /> を HTML5標準の <br> に統一
    - HTMLタグ後のbr除去を preg_replace に簡略化
    - clr_br を strip_br にリネーム、<br/> パターンにも対応
    - pre/script/form の3つのコールバックを1つに統合
    - 変数名を $brbr から $content に変更
    - 正規表現のHTMLタグリストを変数に切り出し
    - README.md を作成

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # brBrbr-PHP7
+
+WordPressの標準フィルター `wpautop` を置き換え、改行をそのまま `<br>` タグに変換するプラグインです。
+
+## 概要
+
+WordPressはデフォルトで `wpautop` フィルターにより改行を `<p>` タグに自動変換しますが、意図しないレイアウト崩れの原因になることがあります。このプラグインは `wpautop` を無効化し、改行を素直に `<br>` へ変換します。
+
+## 機能
+
+- 投稿本文・コメントの改行を `<br>` に変換
+- ブロックレベルHTML要素（`table`, `div`, `ul`, `pre` 等）直後の不要な `<br>` を自動除去
+- `<pre>`, `<script>`, `<form>` ブロック内の `<br>` を除去
+- `<blockquote>` の段落ラッピング対応
+
+## インストール
+
+1. このリポジトリをダウンロードまたは `git clone` する
+2. `brBrbr-PHP7` ディレクトリを `wp-content/plugins/` に配置する
+3. WordPress管理画面の「プラグイン」から有効化する
+
+## 動作要件
+
+- WordPress 4.0 以上
+- PHP 7.0 以上
+
+## オリジナル
+
+[brBrbr](http://camcam.info/wordpress/101/) by CamCam をPHP7対応としてフォークしたものです。
+
+## ライセンス
+
+GPLv2
--- brBrbr-PHP7.php
+++ brBrbr-PHP7.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name:brBrbr-PHP7
 Plugin URI:https://github.com/snickerjp/brBrbr-PHP7
-Description:Line feed is converted to &lt;br /&gt;.
-Version:2.0.1
+Description:Line feed is converted to &lt;br&gt;.
+Version:2.1.0
 Author:snickerjp
 Author URI:https://github.com/snickerjp/
 */
@@ -22,42 +22,37 @@ add_filter('the_content', 'brBrbr');
 remove_filter('comment_text', 'wpautop', 30);
 add_filter('comment_text', 'brBrbr', 30);
 
-function brBrbr($brbr)
+function brBrbr($content)
 {
-    $brbr = str_replace(array(
-        "\r\n",
-        "\r"
-    ), "\n", $brbr); // cross-platform newlines
-    $brbr = str_replace("\n", "<br />\n", $brbr); // cross-platform newlines
-    $brbr = preg_replace_callback('!(</?(?:table|img|thead|tfoot|caption|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|form|textarea|input|blockquote|address|p|math|script|h[1-6])[^>]*>)\s*<br />!', function($m)
-    {
-        return "$1";
-    }, $brbr);
-    $brbr = preg_replace_callback('|<blockquote([^>]*)>|i', function($m)
-    {
-        return "</p>\n<blockquote$1><p>";
-    }, $brbr);
-    $brbr = str_replace('</blockquote>', "</p></blockquote>\n<p>", $brbr);
-    $brbr = preg_replace_callback('/(<pre.*?>)(.*?)<\/pre>/is', function($m)
-    {
-        return clr_br($m[0]);
-    }, $brbr);
-    $brbr = preg_replace_callback('/(<script.*?>)(.*?)<\/script>/is', function($m)
-    {
-        return clr_br($m[0]);
-    }, $brbr);
-    $brbr = preg_replace_callback('/(<form.*?>)(.*?)<\/form>/is', function($m)
-    {
-        return clr_br($m[0]);
-    }, $brbr);
-    $brbr = "<p>\n" . $brbr . "</p>\n";
-    return $brbr;
+    // Normalize newlines
+    $content = str_replace(array("\r\n", "\r"), "\n", $content);
+    $content = str_replace("\n", "<br>\n", $content);
+
+    // Remove <br> immediately after block-level HTML tags
+    $block_tags = 'table|img|thead|tfoot|caption|tbody|tr|td|th|div|dl|dd|dt'
+                . '|ul|ol|li|pre|select|form|textarea|input|blockquote'
+                . '|address|p|math|script|h[1-6]';
+    $content = preg_replace("!(</?(?:{$block_tags})[^>]*>)\s*<br>!", "$1", $content);
+
+    // Blockquote paragraph wrapping
+    $content = preg_replace_callback('|<blockquote([^>]*)>|i', function($m) {
+        return "</p>\n<blockquote{$m[1]}><p>";
+    }, $content);
+    $content = str_replace('</blockquote>', "</p></blockquote>\n<p>", $content);
+
+    // Strip <br> inside pre, script, form blocks
+    $content = preg_replace_callback('/(<(?:pre|script|form).*?>)(.*?)<\/(?:pre|script|form)>/is', function($m) {
+        return strip_br($m[0]);
+    }, $content);
+
+    $content = "<p>\n" . $content . "</p>\n";
+    return $content;
 }
 
 
-function clr_br($str)
+function strip_br($str)
 {
-    $str = str_replace("<br />", "", $str);
+    $str = str_replace(array("<br>", "<br/>", "<br />"), "", $str);
     $str = str_replace('\"', '"', $str);
     return $str;
 }
